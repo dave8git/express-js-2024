87a5ba84b18c16d66fee8b44c07e1112e968e39d
dave8git <dave8git@gmail.com>
2024-09-11 17:49:59 +0200

    Add path.join

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>My first server!</h1>');
+    res.sendFile(path.join(__dirname, './views/index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/about.html'));
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/contact.html'));
+});
+
+app.get('/info', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/info.html'));
+});
+
+app.get('/history', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/history.html'));
 });
 
 app.listen(8000, () => {
