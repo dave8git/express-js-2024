a4d52da0d17ee359001748932c4c1eadce82cb67
dave8git <dave8git@gmail.com>
2024-09-12 19:01:45 +0200

    add static routes and static middleware

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const path = require('path');
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use((req, res, next) => { // use jakby wprowadzi swoją własną nową metodę do wykonania pomiędzy odpowiedzą a zapytaniem (zapytaniem get, post itd...)
     res.show = (name) => {
         res.sendFile(path.join(__dirname, `/views/${name}`));
     };
     next(); 
 });
 
+app.use(express.static(path.join(__dirname, '/public')));
+
 app.get('/', (req, res) => {
-    res.show('index.html');
+    res.show('index.html'); // zanim odpowie co jest pod '/' użyje metody show 
 });
 
 app.get('/about', (req, res) => {
@@ -30,8 +32,16 @@ app.get('/history', (req, res) => {
   res.show('history.html');
 });
 
+// app.get('/style.css', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/style.css'));
+// });
+
+// app.get('/test.jpg', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/test.png'));
+// });
+
 app.use((req, res) => { 
-    res.status(404).send('404 not found...');
+    res.status(404).send('404 not found...'); // nie potrzeba funkcji next() kiedy adres jest niewłaściwy aplikacja nie idzie dalej
 })
 
 app.listen(8000, () => {
