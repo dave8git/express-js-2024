087e17ee32f5de69211fd1e27cc91604d79dccb2
dave8git <dave8git@gmail.com>
2024-09-15 12:39:59 +0200

    Add multer functionality and file input

--- server.js
+++ server.js
@@ -6,7 +6,31 @@ const app = express();
 
 app.engine('.hbs', hbs()); // all files with .hbs extension should be rendered by handlebars engine
 app.set('view engine', '.hbs'); // all our views will be using .hbs extension
+const storage = multer.diskStorage({
+  destination: './uploads',
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '_' + Date.now() + path.extname(file.originalname));
+  }
+});
 
+const upload = multer({
+  storage: storage, 
+  limits: { fileSize: 2000000 },
+  fileFilter: (req, file, cb) => {
+    checkFileType(file, cb); 
+  }
+}).single('myImage');
+
+function checkFileType(file, cb) {
+  const filetypes = /jpeg|jpg|png|gif/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+  if(mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb('Error: Images Only!');
+  }
+}
 // app.use((req, res, next) => { // use jakby wprowadzi swoją własną nową metodę do wykonania pomiędzy odpowiedzą a zapytaniem (zapytaniem get, post itd...)
 //     res.show = (name) => {
 //         res.sendFile(path.join(__dirname, `/views/${name}`));
@@ -41,17 +65,27 @@ app.get('/history', (req, res) => {
 });
 
 app.post('/contact/send-message', (req, res) => {
-  const { author, sender, title, message } = req.body; 
+  console.log('Form is submitted.');
+  upload(req, res, (err) => {
+    if (err) {
+      console.log('File upload error:', err);
+      return res.render('contact', { isError: true, errorMsg: err });
+    }
 
-  if(author && sender && title && message) {
-    res.render('contact', { isSent: true }); 
-  }
-  else {
-    res.render('contact', { isError: true });
-    //res.json(req.body); // res.json method return data in json format (unlike for ex. res.send which returns data in text format)
-  }
+    const { author, sender, title, message } = req.body;
+    console.log('Form data:', req.body);  // This should log the form fields
+    console.log('Uploaded file:', req.file);  // This should log the uploaded file
+
+    if (author && sender && title && message) {
+      console.log('All fields are present');
+      res.render('contact', { isSent: true });
+    } else {
+      console.log('Some fields are missing');
+      res.render('contact', { isError: true });
+    }
+  });
 });
-
+  
 app.get('/hello/:name', (req, res) => {
   //res.send(`Hello ${req.params.id}`);
   res.render('hello', {layout: false, name: req.params.name });
